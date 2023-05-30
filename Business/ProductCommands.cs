26f1fd6bea78f32a877c1d43b648655af0082972
kamen <kamen@DESKTOP-HT8BVVU>
2023-05-30 19:38:35 +0300

    Add project files.

---
+++ Business/ProductCommands.cs
@@ -0,0 +1,61 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class ProductCommands
+    {
+        private ProductContext context { get; set; }
+        public List<Product> Products()
+        {
+            using(context=new ProductContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public Product Show(int id)
+        {
+            using (context=new ProductContext())
+            {
+                return context.Products.Find(id);
+            }
+        }
+        public void Add(Product product)
+        {
+            using (context = new ProductContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public void Update(Product newProduct)
+        {
+            using (context = new ProductContext())
+            {
+                var item = context.Products.Find(newProduct.ID);
+                if (item != null)
+                {
+
+                    context.Entry(item).CurrentValues.SetValues(newProduct);
+                    context.SaveChanges();
+				}
+            }
+        }
+        public void Delete(int id)
+        {
+            using (context = new ProductContext())
+            {
+                var product=context.Products.Find(id);
+                if (product!=null)
+                {
+					context.Products.Remove(product);
+                    context.SaveChanges();
+				}
+            }
+        }
+    }
+}
---
+++ MagazinSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MagazinSite.MvcApplication" Language="C#" %>
